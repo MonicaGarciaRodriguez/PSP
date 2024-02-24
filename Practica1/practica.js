d00db4b0b27b893ae8b79d8a7a7591b2ec559433
Monica Garcia Rodriguez <monigar78@gmail.com>
2024-02-24 01:05:19 +0100

    Update practica.js

--- Practica1/practica.js
+++ Practica1/practica.js
@@ -1,120 +1,41 @@
 
 //importar modulo fs para leer un archivo
-const fs = require('fs').promises;
-
+const fs = require('fs/promises');
 //importar modulo hhtp
 const http = require("http");
 const host = 'localhost';
 const port = 8000;
-
-
 const path = require('path');
-let valor;
-function funcion(parametro) {
-    if (parametro == ".html") {
-        valor = "text/html";
-    }
-    if (parametro == ".css") {
-        valor = "text/css";
-    }
-}
-
+//importar la funcion 
+const {obtenerparametro}   = require("./obtenerparametro.js");
+//crear el servidor
 const server = http.createServer((request, response) => {
+    //obtener mi url
+    const miURL = '.' + request.url;
+    //leer el archivo
+    fs.readFile(miURL)
+        .then(datos => {
+            //obtener la extension del archivo
+            const fichero = path.extname(miURL);
+            //obtener el content-type utilizando un propio modulo
+            const contentType = obtenerparametro(fichero);
+            //imprimir informacion en la consola
+            console.log("Se ha producido una petición");
+            console.log("URL: " + miURL);
+            console.log("Método: " + request.method);
+            console.log("El Content-Type: " + contentType);
+            //enviar respuesta 
+            response.writeHead(200);
+            response.end(datos);
+        })
+        .catch((error) => {
+            console.error(error);
+            response.writeHead(404, {'Content-Type': 'text/html'});
+            response.end('404 No encontrado');
+        });
 
-    if (request.url == '/index.html') {
-        return fs.readFile('./index.html')
-            .then(contents => {
-                parametro = path.extname(request.url);
-                funcion(parametro);
-                console.log("Se ha producido una petición");
-                console.log("URL: " + request.url);
-                console.log("Método: " + request.method);
-                console.log("El Content-Type: " + valor);
-                response.writeHead(200);
-                response.end(contents);
-            })
-            .catch(err => {
-                response.writeHead(404);
-                response.end(err);
-                return;
-            });
-    };
-    if (request.url == '/Pagina1.html') {
-        return fs.readFile('./Pagina1.html')
-            .then(contents => {
-                parametro = path.extname(request.url);
-                funcion(parametro);
-                console.log("Se ha producido una petición");
-                console.log("URL: " + request.url);
-                console.log("Método: " + request.method);
-                console.log("El Content-Type: " + valor);
-                response.writeHead(200);
-                response.end(contents);
-
-            })
-            .catch(err => {
-                response.writeHead(404);
-                response.end(err);
-                return;
-            });
-    };
-    if (request.url == '/Pagina2.html') {
-        return fs.readFile('./Pagina2.html')
-            .then(contents => {
-                parametro = path.extname(request.url);
-                funcion(parametro);
-                console.log("Se ha producido una petición");
-                console.log("URL: " + request.url);
-                console.log("Método: " + request.method);
-                console.log("El Content-Type: " + valor);
-                response.writeHead(200);
-                response.end(contents);
-
-            })
-            .catch(err => {
-                response.writeHead(404);
-                response.end(err);
-                return;
-            });
-    };
-    if (request.url == '/Logo.jpg') {
-        return fs.readFile('./Logo.jpg')
-            .then(contents => {
-                response.setHeader("Content-Type", "text/html");
-                response.writeHead(200);
-                response.end(contents);
-
-            })
-            .catch(err => {
-                response.writeHead(404);
-                response.end(err);
-                return;
-            });
-    };
-    if (request.url == '/style.css') {
-        return fs.readFile('./style.css')
-            .then(contents => {
-                parametro = path.extname(request.url);
-                funcion(parametro);
-                console.log("Se ha producido una petición");
-                console.log("URL: " + request.url);
-                console.log("Método: " + request.method);
-                console.log("El Content-Type: " + valor);
-                response.writeHead(200);
-                response.end(contents);
-
-            })
-            .catch(err => {
-                response.writeHead(404);
-                response.end(err);
-                return;
-            });
-    };
-
-
-});
+    }); 
 
 server.listen(port, host, () => {
     console.log(`Servidor ejecutando en http://${host}:${port}`);
 });
-
