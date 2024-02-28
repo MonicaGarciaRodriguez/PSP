9a59d1aba7a1d95ab60902448e57f0bb81c03e6e
Monica Garcia Rodriguez <monigar78@gmail.com>
2024-02-28 23:24:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,15 +32,75 @@ app.get('/alumnos/:id', (req, res) => {
      res.send('Id recibido ' + req.params.id);
     });
 //insertar uno o varios documentos
-app.post('/alumnos', (rep, res) =>{
-    console.log("nombre: " + req.body.nombre);
-    console.log("fecha: " + req.body.fecha);
-    console.log("modulo: " + req.body.modulo);
-    console.log("grupo: " + req.body.grupo);
-    console.log("ciclo: " + req.body.ciclo);
-    console.log("aula: " + req.body.aula);
-    res.send();
-})
+app.post('/alumnos', (req, res) =>{
+    let alumn = new Alumno({
+        nombre: req.body.nombre,
+        fecha: req.body.fecha,
+        modulo: req.body.modulo,
+        grupo: req.body.grupo,
+        ciclo: req.body.ciclo,
+        aula: req.body.aula
+    });
+    alumn.save().then(
+        result => {
+            console.log("Alumno añadido: ", result );
+            res.send();
+        }
+    ).catch(
+        error => {
+            console.log("Error al añadir alumno", error);
+        }
+    )
+});
+//Obtener todos los documentos que cumplan dos condiciones: 
+//todos los del modulo DAM y ciclo presencial
+app.get('/alumnos', (req, res) =>{
+    Alumno.find({$and:[{modulo: "DAM"}, {ciclo: "Presencial"}]}).then(
+        result => {
+            console.log("El resultado es: ", result);
+            res.send(result);
+        }
+    ).catch(
+        error => {
+            console.log("Error: ", error)
+        } 
+    )
+});
+//Actualizar un documento
+app.put('/alumnos/:id', (req, res) =>{
+    Alumno.findByIdAndUpdate(req.params.id,
+        {$set:{
+            nombre: req.body.nombre,
+            fecha: req.body.fecha,
+            modulo: req.body.modulo,
+            grupo: req.body.grupo,
+            ciclo: req.body.ciclo,
+            aula: req.body.aula
+        } }, {new: true}).then(
+        result => {
+            console.log("Documento actualizado", result);
+            res.send();
+        }
+    ).catch(
+        error => {
+            console.log("Error:", error);
+        }
+    )
+});
+
+//eliminar un documento dado su id
+app.delete('/alumnos/:id', (req, res) =>{
+    Alumno.findByIdAndDelete(req.params.id).then(
+        result => {
+            console.log("Documento eliminado", result);
+            res.send();
+        }
+    ).catch(
+        error => {
+            console.log("Error:", error);
+        }
+    )
+});
  
 // Iniciar el servidor
 app.listen(port, () => {
